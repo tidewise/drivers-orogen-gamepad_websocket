2864ce2844cf905dd1c8ad9ebdc7b0746084b7f8
Jhonas Moura <jhonaspradomoura@gmail.com>
2026-01-23 14:08:55 -0300

    feat: add raw command websocket publisher
    Takes a data from a controldev/RawCommand port, processes it and them
    publishes it to all the websocket clients using the base publisher.

---
+++ tasks/RawCommandWebsocketPublisherTask.cpp
@@ -0,0 +1,79 @@
+/* Generated from orogen/lib/orogen/templates/tasks/Task.cpp */
+
+#include "RawCommandWebsocketPublisherTask.hpp"
+#include "base-logging/Logging.hpp"
+#include "controldev/RawCommand.hpp"
+#include "rtt/FlowStatus.hpp"
+
+using namespace base;
+using namespace gamepad_websocket;
+
+RawCommandWebsocketPublisherTask::RawCommandWebsocketPublisherTask(
+    std::string const& name)
+    : RawCommandWebsocketPublisherTaskBase(name)
+{
+}
+
+RawCommandWebsocketPublisherTask::~RawCommandWebsocketPublisherTask()
+{
+}
+
+/// The following lines are template definitions for the various state machine
+// hooks defined by Orocos::RTT. See RawCommandWebsocketPublisherTask.hpp for more
+// detailed documentation about them.
+bool RawCommandWebsocketPublisherTask::configureHook()
+{
+    if (!RawCommandWebsocketPublisherTaskBase::configureHook())
+        return false;
+
+    m_command_timeout = _command_timeout.get();
+    return true;
+}
+
+bool RawCommandWebsocketPublisherTask::startHook()
+{
+    if (!RawCommandWebsocketPublisherTaskBase::startHook())
+        return false;
+
+    m_command_deadline = Time::now() + m_command_timeout;
+    return true;
+}
+
+void RawCommandWebsocketPublisherTask::updateHook()
+{
+    RawCommandWebsocketPublisherTaskBase::updateHook();
+
+    controldev::RawCommand raw_cmd;
+    auto flow_status = _raw_command.read(raw_cmd);
+    auto now = Time::now();
+    if (flow_status == RTT::NewData) {
+        m_command_deadline = now + m_command_timeout;
+        if (state() != PUBLISHING) {
+            state(PUBLISHING);
+        }
+        m_outgoing_raw_command = raw_cmd;
+    }
+    else if (now > m_command_deadline) {
+        if (state() != INPUT_TIMEOUT) {
+            state(INPUT_TIMEOUT);
+        }
+        return;
+    }
+
+    publishRawCommand();
+}
+
+void RawCommandWebsocketPublisherTask::errorHook()
+{
+    RawCommandWebsocketPublisherTaskBase::errorHook();
+}
+
+void RawCommandWebsocketPublisherTask::stopHook()
+{
+    RawCommandWebsocketPublisherTaskBase::stopHook();
+}
+
+void RawCommandWebsocketPublisherTask::cleanupHook()
+{
+    RawCommandWebsocketPublisherTaskBase::cleanupHook();
+}
---
+++ test/raw_command_websocket_publisher_task_test.rb
@@ -0,0 +1,190 @@
+# frozen_string_literal: true
+
+require "kontena-websocket-client"
+require "json"
+
+using_task_library "gamepad_websocket"
+
+describe OroGen.gamepad_websocket.RawCommandWebsocketPublisherTask do
+    run_live
+
+    attr_reader :task
+    attr_reader :websocket
+
+    def allocate_interface_port
+        server = TCPServer.new(0)
+        server.local_address.ip_port
+    ensure
+        server&.close
+    end
+
+    before do
+        @task = task = syskit_deploy(
+            OroGen.gamepad_websocket.RawCommandWebsocketPublisherTask
+                .deployed_as("websocket")
+        )
+        @port = allocate_interface_port
+        task.properties.port = @port
+        task.properties.endpoint = "/ws"
+        task.properties.command_timeout = Time.at(1)
+
+        @url = "ws://127.0.0.1:#{@port}/ws"
+        @websocket_created = []
+    end
+
+    after do
+        @websocket_created.each do |s|
+            s.ws.close(10) if s.ws&.open?
+            flunk("connection thread failed to quit") unless s.connection_thread.join(10)
+        end
+    end
+
+    describe "publishing raw commands" do
+        before do
+            syskit_configure_and_start(task)
+            @ws = websocket_create
+        end
+
+        it "reflects on the statistics whenever a raw command is published" do
+            actual = expect_execution do
+                syskit_write task.raw_command_port, raw_command([0.5, 1], [1, 0])
+            end.to { have_one_new_sample(task.statistics_port) }
+            assert_equal 1, actual.sockets_statistics.size
+
+            stats = actual.sockets_statistics.first
+            assert_equal 1, stats.sent
+        end
+
+        it "reflects on the statistics specific for each socket whenever a raw command " \
+           "is published" do
+            websocket_create
+            actual = expect_execution do
+                syskit_write task.raw_command_port, raw_command([0.5, 1], [1, 0])
+            end.to { have_one_new_sample(task.statistics_port) }
+            assert_equal 2, actual.sockets_statistics.size
+
+            actual.sockets_statistics.each do |stats|
+                assert_equal 1, stats.sent
+            end
+        end
+
+        it "stops publishing if no new raw command sample arrives for command timeout " \
+           "time" do
+            expect_execution do
+                syskit_write task.raw_command_port, raw_command([0.5, 1], [1, 0])
+            end.to { have_one_new_sample(task.statistics_port) }
+            expect_execution.to { emit(task.input_timeout_event) }
+            messages_after_timeout = @ws.received_messages
+
+            # Sleep half a second to give a chance for the server to publish messages
+            sleep(0.5)
+            assert_equal @ws.received_messages, messages_after_timeout
+        end
+
+        it "resumes publishing if a new raw command sample arrives when the task is in " \
+           "a input timeout state" do
+            expect_execution.to { emit(task.input_timeout_event) }
+            assert @ws.received_messages.empty?
+
+            expect_execution do
+                syskit_write task.raw_command_port, raw_command([0.5, 1], [1, 0])
+            end.to do
+                [have_one_new_sample(task.statistics_port),
+                 emit(task.publishing_event)]
+            end
+
+            assert_websocket_receives_message(@ws)
+        end
+
+        it "publishes the raw command message in a JSON format" do
+            expect_execution do
+                syskit_write task.raw_command_port, raw_command([0.5, 1], [1, 0])
+            end.to do
+                [have_one_new_sample(task.statistics_port),
+                 emit(task.publishing_event)]
+            end
+
+            msg = assert_websocket_receives_message(@ws)
+            msg = msg.delete(:time)
+            assert_equal({ axes: [0.5, 1], buttons: [1, 0], id: "js" }, msg)
+        end
+
+        it "publishes the raw command message in a JSON format to all connected " \
+           "clients" do
+            ws2 = websocket_create
+            expect_execution do
+                syskit_write task.raw_command_port, raw_command([0.5, 1], [1, 0])
+            end.to do
+                [have_one_new_sample(task.statistics_port),
+                 emit(task.publishing_event)]
+            end
+
+            [@ws, ws2].each do |ws_state|
+                msg = assert_websocket_receives_message(ws_state)
+                msg = msg.delete(:time)
+                assert_equal({ axes: [0.5, 1], buttons: [1, 0], id: "js" }, msg)
+            end
+        end
+    end
+
+    def raw_command(axes, buttons, id = "js")
+        { axisValue: axes, buttonValue: buttons, deviceIdentifier: id }
+    end
+
+    def websocket_state_struct
+        @websocket_state_struct ||= Concurrent::MutableStruct.new(
+            :ws, :received_messages, :connection_thread
+        )
+    end
+
+    def websocket_send(state, msg)
+        msg = JSON.generate(msg) unless msg.respond_to?(:to_str)
+        state.ws.send(msg)
+    end
+
+    def websocket_connect(state, event)
+        Kontena::Websocket::Client.connect(@url) do |ws|
+            state.ws = ws
+            event&.set
+            ws.read do |message|
+                state.received_messages += [message]
+            end
+        end
+    rescue Kontena::Websocket::CloseError
+    ensure
+        event&.set
+    end
+
+    def websocket_create(wait: true)
+        s = websocket_state_struct.new(nil, [], nil)
+        event = Concurrent::Event.new if wait
+        s.connection_thread = Thread.new { websocket_connect(s, event) }
+
+        if event && !event.wait(5)
+            unless s.connection_thread.status
+                s.connection_thread.value # raises if the thread terminated with exception
+            end
+            flunk("timed out waiting for the websocket to connect")
+        end
+        @websocket_created << s
+
+        s
+    end
+
+    def websocket_disconnect(state)
+        ws = state.ws
+        ws.close
+    end
+
+    def assert_websocket_receives_message(state, timeout: 3)
+        deadline = Time.now + timeout
+        while Time.now < deadline
+            unless state.received_messages.empty?
+                return JSON.parse(state.received_messages.pop)
+            end
+
+            sleep 0.1
+        end
+        flunk("no messages received in #{timeout}s")
+    end
+end
